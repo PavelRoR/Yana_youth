702865b3ba700aa824bab58c62fc620da3002cd4
PavelRoR <pavelvp2182@yandex.ru>
2017-03-14 02:59:18 +0500

    Вставлена форма,попап на сертификаты

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var gulp = require('gulp'),
+    pug = require('gulp-pug'),
+    autoprefixer = require('autoprefixer'),
+    cssnext = require('cssnext'),
+    precss = require('precss'),
+    postcss = require('gulp-postcss'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create(),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglifyjs'),
+    styl = require('gulp-stylus'),
+    validator = require('gulp-html');
+
+gulp.task('html', function() {
+    return gulp.src('app/index.html')
+        // .pipe(validator())
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('pug', function buildHTML() {
+    return gulp.src('app/pug/index.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('dist/pug'))
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+
+gulp.task('styl', function() {
+    return gulp.src('app/styl/*.styl')
+        .pipe(styl({
+            compress: true
+        }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+
+gulp.task('css', function() {
+    var processors = [
+        autoprefixer({
+            browsers: ['last 5 versions']
+        }),
+        cssnext,
+        precss
+    ];
+    return gulp.src('app/css/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('watch', ['browser-sync', 'pug', 'styl', 'css', 'html'], function() {
+    gulp.watch('app/*.html', ['html'])
+    gulp.watch('app/pug/*.pug', ['pug'])
+    gulp.watch('app/css/*.css', ['css'], browserSync.reload)
+    gulp.watch('app/styl/*.styl', ['styl'])
+    gulp.watch('app/js/*.js', browserSync.reload)
+});
+
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([
+            'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
+            'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+            'app/js/*.js'
+        ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+
+gulp.task('default', ['styl', 'pug', 'css', 'watch', 'browser-sync', 'scripts', 'html']);
